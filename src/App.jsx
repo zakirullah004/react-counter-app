849650bbeb2ccf5949c43407f7a5d5d656dd3b64
zakirullah004 <zk743087@gmail.com>
2026-05-08 02:00:26 +0500

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,28 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 function App() {
   const [count, setCount] = useState(0)
 
+
+  function increamentCount() {
+    setCount((s) => s + 1)
+  }
+  function decreamentCount() {
+    setCount((s) => s - 1)
+  }
+
+  function resetCount(){
+    setCount(0)
+  }
+
   return (
     <div id='mainContainer'>
+      <h1>React</h1>
       <h1>Counter App</h1>
 
       <div id="counterBox">
-        <h2 id='countValue'>10</h2>
-        <div id="reset"><i className="bi bi-arrow-clockwise"></i></div>
-        <div className='btns' id='plus'>+</div>
-        <div className='btns' id='minus'>-</div>
+        <h2 id='countValue'>{count}</h2>
+        <div onClick={resetCount} id="reset"><i className="bi bi-arrow-clockwise"></i></div>
+        <div onClick={increamentCount} className='btns' id='plus'><i class="bi bi-plus"></i></div>
+        <div onClick={decreamentCount} className='btns' id='minus'><i class="bi bi-dash"></i></div>
       </div>
     </div>
   )
